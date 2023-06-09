d7ca64d2475733c59a01645ea652397b1c70e55c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-09 21:22:33 +0000

    Complete tailwind setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,11 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Home</Text>
-      </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl text-center">
+        Open up App.js to start working on your app!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
