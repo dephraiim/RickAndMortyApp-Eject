0f0ec3cda2080ca062850c4067a7d3b5e58d27d6
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-09 16:33:50 +0000

    Add typescript

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Tabs } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={
+        {
+          // API Reference: https://reactnavigation.org/docs/bottom-tab-navigator#options
+        }
+      }
+    />
+  );
+}
