9ff5e979bb0d721450234de10cbab465ca42f3a1
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-09 22:27:06 +0000

    Add customization to bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,47 @@
 import { Tabs } from "expo-router";
+import {
+  MapPinIcon,
+  UserCircleIcon,
+  FilmIcon,
+} from "react-native-heroicons/outline";
 
 export default function Layout() {
   return (
     <Tabs
-      screenOptions={
-        {
-          // API Reference: https://reactnavigation.org/docs/bottom-tab-navigator#options
-        }
-      }
-    />
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#52af38",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return <UserCircleIcon color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="location"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return <MapPinIcon color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="episode"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return <FilmIcon color={color} size={size} />;
+          },
+        }}
+      />
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl text-center">
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl text-morty text-center">
         Open up App.js to start working on your app!
       </Text>
     </View>
