78785500cb3e0974b9a5380034d0b61ea865406c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-10 07:58:23 +0000

    Add status section

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View, TextInput } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
+  const status = ["Alive", "Dead", "Unknown"];
+
   return (
     <SafeAreaView className="flex-1 bg-black">
       {/* Search */}
@@ -19,6 +26,33 @@ export default function Page() {
         <TouchableOpacity className="border-2 border-morty p-2 rounded-xl bg-morty">
           <MagnifyingGlassIcon size={20} color="#000" />
         </TouchableOpacity>
+      </View>
+
+      {/* Status */}
+      <View className="mx-4 my-2">
+        <View>
+          <Text className="text-white text-3xl font-semibold">Status</Text>
+        </View>
+
+        <FlatList
+          data={status}
+          renderItem={(a) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  console.log(a.item);
+                }}
+              >
+                <View className="flex flex-row items-center justify-between  border-2 border-morty my-2 mr-3 rounded-xl">
+                  <Text className="text-morty p-2 py-1 text-base">
+                    {a.item}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+          horizontal
+        />
       </View>
     </SafeAreaView>
   );
