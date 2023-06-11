a77415a54556ece3a49bc36834e1a13054c001e0
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-11 09:35:03 +0000

    Eject expo

--- app/(index)/index.tsx
+++ app/(index)/index.tsx
@@ -9,6 +9,7 @@ import {
   ActivityIndicator,
   SafeAreaView,
   useWindowDimensions,
+  ScrollView,
 } from "react-native";
 import {
   ArrowSmallLeftIcon,
@@ -43,183 +44,180 @@ export default function Page() {
   return (
     <SafeAreaView className="flex-1 bg-black">
       {/* Search */}
-      <View className="flex flex-row items-center justify-center">
-        <TextInput
-          placeholder="Search for a character..."
-          placeholderTextColor="#515151"
-          style={{
-            lineHeight: 20,
-          }}
-          className="text-white text-lg m-4 mr-2 p-4 py-2 flex-1 border-2 border-morty rounded-xl"
-        />
-        <TouchableOpacity
-          className={`border-2 border-morty p-2 rounded-xl mr-4 bg-morty ${
-            isAndroid && "p-3"
-          }`}
-        >
-          <MagnifyingGlassIcon strokeWidth={3} size={20} color="#000" />
-        </TouchableOpacity>
-      </View>
-
-      {/* Status */}
-      <View className="mx-4 my-2">
-        <View>
-          <Text className="text-white text-3xl font-semibold">Status</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View className="flex flex-row items-center justify-center">
+          <TextInput
+            placeholder="Search for a character..."
+            placeholderTextColor="#515151"
+            style={{
+              lineHeight: 20,
+            }}
+            className="text-white text-lg m-4 mr-2 p-4 py-2 flex-1 border-2 border-morty rounded-xl"
+          />
+          <TouchableOpacity
+            className={`border-2 border-morty p-2 rounded-xl mr-4 bg-morty ${
+              isAndroid && "p-3"
+            }`}
+          >
+            <MagnifyingGlassIcon strokeWidth={3} size={20} color="#000" />
+          </TouchableOpacity>
         </View>
 
-        <View className="flex flex-row justify-between items-center">
-          <FlatList
-            data={status}
-            renderItem={(a) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    setPage(1);
-                    setStatusState(a.item.toLocaleLowerCase());
-                  }}
-                >
-                  <View
-                    className={`flex flex-row items-center justify-between  border-2 border-morty my-2 mr-3 rounded-xl ${
-                      statusState !== a.item.toLocaleLowerCase() && "opacity-50"
-                    }`}
+        {/* Status */}
+        <View className="mx-4 my-2">
+          <View>
+            <Text className="text-white text-3xl font-semibold">Status</Text>
+          </View>
+
+          <View className="flex flex-row justify-between items-center">
+            <FlatList
+              data={status}
+              renderItem={(a) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setPage(1);
+                      setStatusState(a.item.toLocaleLowerCase());
+                    }}
                   >
-                    <Text className="text-morty p-2 py-1 text-base">
-                      {a.item}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-            horizontal
-          />
-          {statusState !== "" && (
-            <TouchableOpacity
-              onPress={() => {
-                setPage(1);
-                setStatusState("");
+                    <View
+                      className={`flex flex-row items-center justify-between  border-2 border-morty my-2 mr-3 rounded-xl ${
+                        statusState !== a.item.toLocaleLowerCase() &&
+                        "opacity-50"
+                      }`}
+                    >
+                      <Text className="text-morty p-2 py-1 text-base">
+                        {a.item}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                );
               }}
-            >
-              <Text className="text-morty">Clear</Text>
-            </TouchableOpacity>
+              horizontal
+            />
+            {statusState !== "" && (
+              <TouchableOpacity
+                onPress={() => {
+                  setPage(1);
+                  setStatusState("");
+                }}
+              >
+                <Text className="text-morty">Clear</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+
+        <View className="mx-4 my-5">
+          <View>
+            <Text className="text-white text-3xl font-semibold">
+              Characters
+            </Text>
+          </View>
+
+          {isLoading && (
+            <View className="my-52">
+              <ActivityIndicator color="#fff" size="large" />
+            </View>
+          )}
+
+          {/* TODO: Error Component */}
+
+          <View className="flex flex-row flex-wrap">
+            {!isLoading &&
+              data &&
+              !error &&
+              data.results.map((item) => {
+                return (
+                  <TouchableOpacity
+                    key={item.id + item.name}
+                    className="w-1/2"
+                    onPress={() => {
+                      const params = {
+                        id: item.id,
+                        name: item.name,
+                        image:
+                          item.image.split("/")[
+                            item.image.split("/").length - 1
+                          ],
+                        status: item.status,
+                        species: item.species,
+                        type: item.type,
+                        gender: item.gender,
+                        originName: item.origin.name,
+                        originId:
+                          item.origin.url.split("/")[
+                            item.origin.url.split("/").length - 1
+                          ],
+                        locationName: item.location.name,
+                        locationId:
+                          item.location.url.split("/")[
+                            item.location.url.split("/").length - 1
+                          ],
+                      };
+
+                      router.push({
+                        pathname: `/character/${item.id}`,
+                        params,
+                      });
+                    }}
+                  >
+                    <View className="mr-3 ml-1 mt-4">
+                      <Image
+                        style={{
+                          width: (w * 0.85) / 2,
+                          height: (w * 0.85) / 2,
+                          borderRadius: 10,
+                        }}
+                        source={{ uri: item.image }}
+                        loadingIndicatorSource={defaultImage}
+                      />
+                      <View className="flex flex-row items-center justify-between mt-1">
+                        <Text className="text-white text-lg font-semibold">
+                          {truncateString(item.name, 15)}
+                        </Text>
+                        <View
+                          className={`w-2 h-2 rounded-full ${
+                            item.status === "Alive"
+                              ? "bg-green-600"
+                              : item.status === "Dead"
+                              ? "bg-red-600"
+                              : "bg-gray-700"
+                          }`}
+                        />
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                );
+              })}
+          </View>
+          {!isLoading && (
+            <View className="flex flex-row justify-between mt-10 mx-2">
+              <TouchableOpacity
+                onPress={() => {
+                  if (data.info.prev) {
+                    setPage(page - 1);
+                  }
+                }}
+              >
+                <ArrowSmallLeftIcon color="#fff" size={30} strokeWidth={3} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  if (data.info.next) {
+                    setPage(page + 1);
+                  }
+                }}
+              >
+                <ArrowSmallRightIcon color="#fff" size={30} strokeWidth={3} />
+              </TouchableOpacity>
+            </View>
           )}
         </View>
-      </View>
-
-      <View className="mx-4 my-5">
-        <View>
-          <Text className="text-white text-3xl font-semibold">Characters</Text>
-        </View>
-
-        {isLoading && (
-          <View className="mt-60">
-            <ActivityIndicator color="#fff" size="large" />
-          </View>
-        )}
-
-        {/* TODO: Error Component */}
-
-        {!isLoading && data && !error && (
-          <FlatList
-            data={data.results}
-            style={{ marginTop: 10 }}
-            renderItem={({ item }) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    const params = {
-                      id: item.id,
-                      name: item.name,
-                      image:
-                        item.image.split("/")[item.image.split("/").length - 1],
-                      status: item.status,
-                      species: item.species,
-                      type: item.type,
-                      gender: item.gender,
-                      originName: item.origin.name,
-                      originId:
-                        item.origin.url.split("/")[
-                          item.origin.url.split("/").length - 1
-                        ],
-                      locationName: item.location.name,
-                      locationId:
-                        item.location.url.split("/")[
-                          item.location.url.split("/").length - 1
-                        ],
-                    };
-
-                    router.push({
-                      pathname: `/character/${item.id}`,
-                      params,
-                    });
-                  }}
-                >
-                  <View className="mr-3 ml-1 mt-4">
-                    <Image
-                      style={{
-                        width: (w * 0.85) / 2,
-                        height: (w * 0.85) / 2,
-                        borderRadius: 10,
-                      }}
-                      source={{ uri: item.image }}
-                      loadingIndicatorSource={defaultImage}
-                    />
-                    <View className="flex flex-row items-center justify-between mt-1">
-                      <Text className="text-white text-lg font-semibold">
-                        {truncateString(item.name, 15)}
-                      </Text>
-                      <View
-                        className={`w-2 h-2 rounded-full ${
-                          item.status === "Alive"
-                            ? "bg-green-600"
-                            : item.status === "Dead"
-                            ? "bg-red-600"
-                            : "bg-gray-700"
-                        }`}
-                      />
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-            keyExtractor={(item) => item.id.toString() + item.name}
-            contentContainerStyle={{ paddingBottom: h * 0.3 }}
-            showsVerticalScrollIndicator={false}
-            numColumns={2}
-            ListFooterComponent={() => {
-              return (
-                <View className="flex flex-row justify-between mt-10 mx-2">
-                  <TouchableOpacity
-                    onPress={() => {
-                      if (data.info.prev) {
-                        setPage(page - 1);
-                      }
-                    }}
-                  >
-                    <ArrowSmallLeftIcon
-                      color="#fff"
-                      size={30}
-                      strokeWidth={3}
-                    />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={() => {
-                      if (data.info.next) {
-                        setPage(page + 1);
-                      }
-                    }}
-                  >
-                    <ArrowSmallRightIcon
-                      color="#fff"
-                      size={30}
-                      strokeWidth={3}
-                    />
-                  </TouchableOpacity>
-                </View>
-              );
-            }}
-          />
-        )}
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
